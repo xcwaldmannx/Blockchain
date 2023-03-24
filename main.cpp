026f623604fcf472686483557b66f5d5feb84aae
Christian Waldmann <xcwaldmannx@gmail.com>
2023-03-24 17:30:44 -0400

    first commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#include <string>
+
+int main() {
+
+	std::cout << "fuck" << std::endl;
+
+	std::string name;
+	std::cin >> name;
+
+	std::cout << name << std::endl;
+	std::cin >> name;
+
+	return 0;
+}
